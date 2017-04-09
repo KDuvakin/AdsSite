b7cda088ac6100b260d1afe14500647168c6fe11
Konstantin Duvakin <Konstantin Duvakin>
2017-04-09 12:42:13 +0300

    Add project files.

---
+++ sl-lv/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sl_lv.MvcApplication" Language="C#" %>
---
+++ sl-lv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sl_lv.Startup))]
+namespace sl_lv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
